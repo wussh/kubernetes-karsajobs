d1f99d460c2ca53aead934d763dd72f768f14030
wussh <andrianta.321@gmail.com>
2023-10-07 12:36:02 +0700

    add detailed comment

--- kubectl.sh
+++ kubectl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to apply individual YAML files in a directory
 apply_directory() {
   local directory="$1"
   echo "Applying YAML files in $directory..."
@@ -9,13 +8,8 @@ apply_directory() {
   done
 }
 
-# Apply YAML files in the mongodb directory
 apply_directory "mongodb"
-
-# Apply YAML files in the backend directory
 apply_directory "backend"
-
-# Apply YAML files in the frontend directory
 apply_directory "frontend"
 
-echo "Finished applying all YAML files."
+echo "Finished apply all"
